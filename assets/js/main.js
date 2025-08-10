c6266e07be5188947024004be40f008d28f1bdea
Nguyen Xuan Hiep <92458329+HiIamHiep@users.noreply.github.com>
2025-08-10 10:56:17 +0900

    Add header scroll position sticky

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Scroller for header
+const header = $("#sticky-header");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
